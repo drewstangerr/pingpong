6205faaeedd982dddd780bc10b6bab1e608cf957
Drew Stanger <drew.stanger@gmail.com>
2019-06-29 17:43:58 -0700

    rough draft of scripts

---
+++ README.md
@@ -0,0 +1,40 @@
+# Ping Pong Project!
+
+#### _Week 9 Arrays and Looping Independent project_
+
+#### By _Drew Stanger_
+
+## Description
+
+_{This web application takes a number from a user and returns a range of numbers from 1 to the chosen number with the following exceptions:_
+
+## Specs
+| Behavior        | Input           | Outcome  |
+| ------------- |:-------------:| -----:|
+| Numbers divisible by 15 are replaced by the word "pingpong"| 15 | pingpong |
+| Numbers divisible by 5 are replaced by the word "pong" | 5 | pong
+| Numbers divisible by 3 are replaced by the word "ping" | 3 | ping |
+| Numbers not divisible by 15, 5, or 3 are not altered| 1 | 1 |
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open HTML file_
+* _Open web browser_
+* _GitHub Pages link:https://github.com/drewstangerr/pingpong
+
+## Support and contact details
+
+Drew Stanger
+drew.stanger@icloud.com
+
+## Technologies Used
+*_HTML_
+*_Bootstrap_
+*_CSS_
+*_Javascript_
+*_jQuery_
+
+### License
+
+Copyright (c) 2019 **_Drew Stanger_**
---
+++ scripts.js
@@ -0,0 +1,44 @@
+var input;
+var number= [];
+
+var pp= function (input) {
+  if (input> 250) {
+    alert("Enter a smaller number, a number <=250")
+  } else {
+    for (i=0 i<=input; i+1) {
+      number.push(i);
+    }
+  }
+}
+
+var pingpong= function (){
+  number.map(function(numbers){
+    if (numbers===0) {
+      $("#result").append("<li>" + "Enjoy:" + "</li>");
+    } else if (numbers % 15 === 0){
+      $("#result").append("<li>" + "pingpong:" + "</li>");
+    } else if (numbers % 5 === 0){
+      $("#result").append("<li>" + "pong:" + "</li>");
+    } else if (numbers % 3 === 0){
+      $("#result").append("<li>" + "ping:" + "</li>");
+    } else {
+      $("#result").append("<li>" + numbers + "</li>")
+    }}
+    }
+  }
+)}
+
+$(document).ready(function(){
+  $("form#number").submit(function(event){
+    event.preventDefault();
+    input= parseInt($("input#number").val());
+    $("result").empty();
+    number= [];
+    pp (input);
+    pingpong ();
+
+    $('#result').click(function() {
+      location.reload();
+    });
+  });
+});
